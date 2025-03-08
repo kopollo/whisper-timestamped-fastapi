ee06269682ccf83e6f4eaceeadf15b536678f5eb
Andrey Samoylenko <andreysamsaa@gmail.com>
2025-03-08 12:42:35 +0300

    feat: add logging

--- client.py
+++ client.py
@@ -13,8 +13,13 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 import whisper_timestamped
 
-path = "files/test.wav"
 model = whisper_timestamped.load_model(cfg['model'], device=cfg['device'], download_root='.cache')
+
+import logging
+import traceback
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def process_audio(audio_path: str):
@@ -31,15 +36,16 @@ async def process_audio(audio_path: str):
 async def transcribe(file: UploadFile = File(...)):
     try:
         file_path = os.path.join(UPLOAD_DIR, file.filename)
-        print(file_path)
+        logger.info(f"Try to load file: {file_path}")
         with open(file_path, "wb") as f:
             f.write(await file.read())
 
         result = await process_audio(file_path)
         return JSONResponse(content=result)
     except Exception as e:
+        logger.error("An error occurred: %s", traceback.format_exc())
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 if __name__ == "__main__":
-    uvicorn.run("client:app", host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run("client:app", host="0.0.0.0", port=9000, workers=1)
